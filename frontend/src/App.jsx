3d56d14797e177447e15b50ebe02a66a32b335b9
Pottri <pottrir22@gmail.com>
2025-11-14 21:34:03 +0530

    Added event

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,7 +33,6 @@ export default function App() {
 
   // Hide navbar on login + signup pages
   const hideNavbarPaths = ["/login", "/signup","/forgot-password"];
-  const hideNavbarPaths = ["/login", "/signup"];
   const shouldShowNavbar = !hideNavbarPaths.includes(location.pathname) && profile;
 
   return (
